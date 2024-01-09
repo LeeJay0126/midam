cbb0105f21ab35950be471e52af7a92b4fec3fb4
LeeJay0126 <topaz9889@gmail.com>
2024-01-08 22:46:49 -0800

    Menu changes

---
+++ src/Pages/Menu/MenuList/DrinkTypeList.js
@@ -0,0 +1,33 @@
+
+const DrinkTypeList = [
+    ["BEER", [
+        ["question.jpeg", "Sapporo", "$6", "Sapporo"],
+        ["question.jpeg", "Asahi", "$6", "Sapporo"],
+        ["question.jpeg", "Cass", "$6", "Sapporo"],
+    ]],
+    ["KOREAN LIQUOR", [
+        ["question.jpeg", "Soju", "$17", "Soju"],
+        ["question.jpeg", "Local Soju", "$14", "Local Soju"],
+        ["question.jpeg", "Flavour Soju", "$17", "Flavour Soju"],
+        ["question.jpeg", "Chung Ha", "$12", "Chung Ha"],
+        ["question.jpeg", "Suljoongme", "$16", "Suljoongme"],
+        ["question.jpeg", "Bokbunja", "$18", "Bokbunja"],
+    ]],
+    ["RICE WINE", [
+        ["question.jpeg", "Original Ricewine", "$15", "Original Rice Wine"],
+        ["question.jpeg", "Chestnut Ricewine", "$15", "Chestnut Rice Wine"],
+        ["question.jpeg", "Citron Ricewine", "$15", "Citron Rice Wine"],
+        ["question.jpeg", "Strawberry Ricewine", "$15", "Strawberry Rice Wine"],
+    ]],
+    ["BEVERAGE", [
+        ["question.jpeg", "Milkis", "$3.5", "Milkis"],
+        ["question.jpeg", "Grape Bongbong", "$3.5", "Grape Bongbong"],
+        ["question.jpeg", "Rice Punch", "$3.5", "Rice Punch"],
+        ["question.jpeg", "Pear Juice", "$3.5", "Pear Juice"],
+        ["question.jpeg", "Coke", "$2", "Coke"],
+        ["question.jpeg", "Diet Coke", "$2", "Diet Coke"],
+        ["question.jpeg", "7up", "$2", "7up"],
+        ["question.jpeg", "Iced Tea", "$2", "Iced Tea"],
+        ["question.jpeg", "Ginger Ale", "$2", "Ginger Ale"],
+    ]]
+];
