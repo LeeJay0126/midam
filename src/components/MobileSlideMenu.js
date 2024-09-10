7d8825b845665161e58ec29fd1fa46327b4bbead
Jay Seung Yeon Lee <topaz9889@gmail.com>
2024-09-10 14:59:05 +0900

    Mobile Slide menu links and styling

--- src/components/MobileSlideMenu.js
+++ src/components/MobileSlideMenu.js
@@ -1,46 +1,70 @@
-import HomeMenu from '../Images/Logos/icon _home_.png';
-import Menu from '../Images/Logos/icon _fork spoon_.png';
-import Contact from '../Images/Logos/icon _phone_.png';
-import './Header.css';
-
+import HomeMenu from "../Images/Logos/icon _home_.png";
+import Menu from "../Images/Logos/icon _fork spoon_.png";
+import Contact from "../Images/Logos/icon _phone_.png";
+import Location from "../Images/Logos/icon _location pin_.png";
+import "./Header.css";
+import { HashLink } from "react-router-hash-link";
 
 const MobileSlideMenu = (props) => {
-    let active = props.active;
-    return (
-        <section className={active ? "MobileSlideMenu MBSlideMenuActive" : "MobileSlideMenu"}>
-            <main className='MSContainer'>
-                <div className="MSOptionContainer">
-                    <img src={HomeMenu} className='MSIcon' alt='Mobile Slide Menu Home Button' />
-                    <h2 className='MSLabel'>
-                        HOME
-                    </h2>
-                </div>
-                <div className="MSOptionContainer">
-                    <img src={Menu} className='MSIcon' alt='Mobile Slide Menu Menu Button' />
-                    <h2 className='MSLabel'>
-                        MENU
-                    </h2>
-                </div>
-                <div className="MSOptionContainer">
-                    <img src={Contact} className='MSIcon' alt='Mobile Slide Menu Contact Button' />
-                    <h2 className='MSLabel'>
-                        CONTACT
-                    </h2>
-                </div>
-            </main>
-            <aside className='tempInfo'>
-                <p className='tempInfoP'>
-                    3211 W Broadway, Vancouver, BC
-                </p>
-                <p className='tempInfoP'>
-                    V6K 2H5
-                </p>
-                <p className='tempInfoP'>
-                    (604) 714-0233
-                </p>
-            </aside>
-        </section>
-    );
+  let active = props.active;
+  return (
+    <section
+      className={
+        active ? "MobileSlideMenu MBSlideMenuActive" : "MobileSlideMenu"
+      }
+    >
+      <main className="MSContainer">
+        <div className="MSOptionContainer">
+          <img
+            src={HomeMenu}
+            className="MSIcon"
+            alt="Mobile Slide Menu Home Button"
+          />
+          <h2 className="MSLabel">
+            <HashLink smooth to="/">
+              Home
+            </HashLink>
+          </h2>
+        </div>
+        <div className="MSOptionContainer">
+          <img
+            src={Menu}
+            className="MSIcon"
+            alt="Mobile Slide Menu Menu Button"
+          />
+          <h2 className="MSLabel">
+            <HashLink smooth to="/broadway/menu">
+              Menu
+            </HashLink>
+          </h2>
+        </div>
+        <div className="MSOptionContainer">
+          <img
+            src={Contact}
+            className="MSIcon"
+            alt="Mobile Slide Menu Contact Button"
+          />
+          <h2 className="MSLabel">
+            <HashLink smooth to="/#contact">
+              Contact
+            </HashLink>
+          </h2>
+        </div>
+        <div className="MSOptionContainer">
+          <img
+            src={Location}
+            className="MSIcon"
+            alt="Mobile Slide Menu Contact Button"
+          />
+          <h2 className="MSLabel">
+            <HashLink smooth to="/#location">
+              Location
+            </HashLink>
+          </h2>
+        </div>
+      </main>
+    </section>
+  );
 };
 
 export default MobileSlideMenu;
